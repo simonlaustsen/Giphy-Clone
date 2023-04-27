30a3607918b2b6eab992911d3ce76aa8b46f73c0
Simon Laustsen <sla@perfion.com>
2023-04-27 12:02:00 +0200

    reduce limit

--- giphy-clone/src/app/search-bar/search-bar.service.ts
+++ giphy-clone/src/app/search-bar/search-bar.service.ts
@@ -8,7 +8,7 @@ export class SearchBarService {
   constructor(public http: HttpClient) {}
 
   public getImages(query: string) {
-    const url = `https://api.giphy.com/v1/stickers/search?q=${query}&limit=10&rating=g&api_key=1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq`;
+    const url = `https://api.giphy.com/v1/stickers/search?q=${query}&limit=3&rating=g&api_key=1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq`;
     return this.http.get(url);
   }
 }
