4345f767a02f09bd55d44fcc5d48e9430be62f54
Simon Laustsen <sla@perfion.com>
2023-04-27 12:01:16 +0200

    fix: import

--- giphy-clone/src/app/app.module.ts
+++ giphy-clone/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ResultsComponent } from './results/results.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [AppComponent, SearchBarComponent, ResultsComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- giphy-clone/src/app/search-bar/search-bar.component.ts
+++ giphy-clone/src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { SearchBarService } from './search-bar.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -14,16 +15,16 @@ export class SearchBarComponent {
   });
 
   constructor(
-    private fb: FormBuilder
-  ) // private searchBarService: SearchBarService
-  {}
+    private fb: FormBuilder,
+    private searchBarService: SearchBarService
+  ) {}
 
   onSearch() {
     const query = this.searchForm.value.search as string;
     console.log('📣', query);
-    // this.searchBarService.getImages(query).subscribe((response) => {
-    //   console.log('📣', response);
-    // });
+    this.searchBarService.getImages(query).subscribe((response) => {
+      console.log('📣', response);
+    });
 
     // https://api.giphy.com/v1/stickers/search?q=cat&limit=10&rating=g&api_key=1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq
   }
